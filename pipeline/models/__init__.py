2b234bced9b6f27d8ddacb4cb07bf39c02f94044
LEEJEONGMIN <1935138@gmail.com>
2025-10-06 19:07:43 +0900

    feat: add ensemble model training code

--- pipeline/models/__init__.py
+++ pipeline/models/__init__.py
@@ -5,10 +5,11 @@ This module contains model wrappers and ensemble implementations.
 
 from .xgboost_model import XGBoostModel
 from .lightgbm_model import LightGBMModel
-from .ensemble import EnsembleModel
+from .ensemble import EnsembleModel, VotingEnsemble
 
 __all__ = [
     'XGBoostModel',
     'LightGBMModel',
     'EnsembleModel',
+    'VotingEnsemble',
 ]
